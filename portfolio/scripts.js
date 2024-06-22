f5fcb863eee966d61adf963ee1cfe2fcf346aafa
aaaadityapal <aaadityapal69@gmail.com>
2024-06-22 13:58:23 +0530

    Add files via upload

---
+++ portfolio/scripts.js
@@ -0,0 +1,19 @@
+// scripts.js
+document.addEventListener('DOMContentLoaded', () => {
+    const observerOptions = {
+        threshold: 0.1
+    };
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('animate__animated', 'animate__fadeIn');
+                observer.unobserve(entry.target);
+            }
+        });
+    }, observerOptions);
+
+    document.querySelectorAll('section').forEach(section => {
+        observer.observe(section);
+    });
+});
